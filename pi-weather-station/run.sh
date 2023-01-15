a3887c2ce0c46f6b91fa0a59b5872243fad76a3b
Paul "Rohja" LESELLIER <rohja@rohja.com>
2023-01-15 15:17:59 +0100

    Fetch config

--- pi-weather-station/run.sh
+++ pi-weather-station/run.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/with-contenv bashio
 
+echo "Generating config file..."
+
+MAPS_API_KEY=$(bashio::config 'maps_api_key')
+WEATHER_API_KEY=$(bashio::config 'weather_api_key')
+GEOLOCATION_API_KEY=$(bashio::config 'geolocation_api_key')
+
+LATITUDE=$(bashio::config 'latitude')
+LONGITUDE=$(bashio::config 'longitude')
+
+cat << EOF > /app/settings.json
+{"weatherApiKey":"${WEATHER_API_KEY}",
+ "mapApiKey":"${MAPS_API_KEY}",
+ "reverseGeoApiKey":"${GEOLOCATION_API_KEY}",
+ "startingLat":"${LATITUDE}",
+ "startingLon":"${LONGITUDE}"}
+EOF
+
 echo "Starting Pi Weather Station..."
 
 cd /app && npm start
